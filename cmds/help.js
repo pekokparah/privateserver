c6c1db386747057c7b6848b9843df133538f9404
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-11 23:05:51 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,8 +3,14 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
   let patrick = new Discord.RichEmbed()
-.setTitle(':palm_tree: Sorry if the service patrick is not complete :palm_tree:  ')
-.setDescription('`-clear`   = To clear chats on channel ! \n`-say`   = repeat the word you mentioned \n`-botinfo`   = know the identity of the bot ! \n`-report`   = to report violators ! \n`-ping`    = to test your ping !\n`-serverinfo`   = provide data from the server ! \n`-vote`   = judging a persons work or opinion ! \n`-avatar`  = For photos view your profile ! \n\n`-invite`  = To invite this bot to your server !\n`-info`    = To find out more info about Patrick ! \n`-Update`  = To check for new BOT update feature !\n \nif you need help use the command: `-help`')
+.setTitle(':palm_tree: Hello, i am patrick :P')
+.addDescription('I`m the bot of the bikini bottom')
+.addField('Anime:','`neko`', true)
+.addField('Core:','`help``ping``invite`', true)
+.addField('Moderation:','`clear``welcome`', true)
+.addField('Utility:','`avatar`', true)
+.addField('Fun:','`say`', true)
+.addField('Image:','`cat``dog`', true)
 .setColor('#00FF00')
 .setThumbnail(bicon)
 .setFooter('©Beta | By: Patrick Poppy | 2018')
