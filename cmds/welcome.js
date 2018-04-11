6fbb08896fec2bc5f7a9f901e64b782dc1abc59f
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-11 22:30:51 +0700

    Create welcome.js

---
+++ cmds/welcome.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+let ping = Date.now() - message.createdTimestamp
+let embed = new Discord.RichEmbed()
+.setTitle(':bow:  Welcome Command !')
+.addField('how to use welcome commands?',create a channel called `welcome-leave`', true)
+.addField('How does it work?','`will automatically welcome new members or members who have been out of the server`', true')
+.setColor('#00FF00')
+.setFooter('©Beta | By: Patrick Poppy | 2018')
+message.channel.send({embed})
+}
+
+exports.help = {
+  name: "welcome"
+}
