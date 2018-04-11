c21ff4a99a5a64039b0d6e7a5d812f35450cedcb
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-11 22:16:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,18 +45,17 @@ bot.on("message", async autoresponder => {
 
 bot.on('guildMemberAdd', member => {
     let channel = member.guild.channels.find('name', 'welcome-leave');
-    let memberavatar = member.user.avatarURL
+    let memberavatar = member.user.avatarURL()
         if (!channel) return;
         let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setThumbnail(memberavatar)
         .addField(':bust_in_silhouette: | name : ', `${member}`)
         .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
-        .addField(':id: | User :', "**[" + `${member.id}` + "]**")
         .addField(':family_mwgb: | Your are the member', `${member.guild.memberCount}`)
         .addField("Name", `<@` + `${member.id}` + `>`, true)
         .addField('Server', `${member.guild.name}`, true )
-        .setFooter(`**${member.guild.name}**`)
+        .setFooter(`${member.guild.name}`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
@@ -70,16 +69,15 @@ bot.on('guildMemberAdd', member => {
 
 bot.on('guildMemberRemove', member => {
     let channel = member.guild.channels.find('name', 'welcome-leave');
-    let memberavatar = member.user.avatarURL
+    let memberavatar = member.user.avatarURL()
         if (!channel) return;
         let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setThumbnail(memberavatar)
-        .addField('Name:', `${member}`)
+        .addField(':bust_in_silhouette: | Name:', `${member}`)
         .addField('Has Let the Server', 'Good Bye !')
-        .addField('Bye Bye :(', 'We will all miss you!')
-        .addField('The server now as', `${member.guild.memberCount}` + " members")
-        .setFooter(`**${member.guild.name}**`)
+        .addField(':family_mwgb: | The server now as', `${member.guild.memberCount}` + " members")
+        .setFooter(`${member.guild.name}`)
         .setTimestamp()
 
         channel.sendEmbed(embed);
