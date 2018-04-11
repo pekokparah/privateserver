fba05695deac92d7c8b9e2b34e769599ca8fc2e9
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-11 23:28:25 +0700

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
+   let insert = args.join(" ");
+      if (!insert) {
+          message.channel.send('🚫 usage : `-say <text>`');
+          return;
+      }
   message.delete();
   let botmessage = args.join(" ");
   message.channel.send(botmessage);
