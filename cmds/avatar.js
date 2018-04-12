9621640b788e4d529d9b75da16b0a414462ced51
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-12 19:47:11 +0700

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
   let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!member) return message.channel.send("🚫 usage : `-avatar <@name>`");
+  if(!member) return message.channel.send("🚫 usage : `*avatar <@name>`");
 
     let embed = new Discord.RichEmbed()
         .setTitle(`${member.user.tag}`)
