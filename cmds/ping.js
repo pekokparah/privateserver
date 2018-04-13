19aa4cafee8333597cb9a438809c875396a82a58
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-13 19:22:09 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -5,7 +5,7 @@ let ping = Date.now() - message.createdTimestamp
 let embed = new Discord.RichEmbed()
 .setTitle(':dragon: Patrick Heard!')
 .addField('Signal :signal_strength:',`${ping}ms`, true)
-.setColor('#00FF00')
+        .setColor("#9A2EFE")
  .setFooter('©Beta | By: Qyto | 2018')
 message.channel.send({embed})
 }
