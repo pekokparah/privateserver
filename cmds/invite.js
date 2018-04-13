e463a4b9ac2861664ea9df0440bab7e561382525
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-13 19:21:56 +0700

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
 let embed = new Discord.RichEmbed()
 .setTitle('want to invite this bot to your server? :robot:')
 .addField('Link','[Klick Here](https://discordapp.com/oauth2/authorize?client_id=434304339268337665&scope=bot&permissions=8)', true)
-.setColor('#00FF00')
+        .setColor("#9A2EFE")
 .setFooter('@Beta | By: Patrick Poppy | 2018')
 message.channel.send({embed})
 
