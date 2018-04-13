47294c1112659d927b99aa29aa29bcf4b20ca2b8
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 00:28:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,8 +41,8 @@ bot.on("message", async autoresponder => {
         if (autoresponder.content.startsWith(prefix)) return;
     
     if (autoresponder.content === `<@${bot.user.id}>`) {
+    message.react('👍');
     return autoresponder.channel.send(`Hi ${sender},` + ' use this command ``c.help`` ')
-           message.react('👍');
     }
         
 });
