e877e8d770d4d56767b67917b19972ef21511498
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-13 19:20:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ bot.on('guildMemberAdd', member => {
     let memberavatar = member.user.displayAvatarURL;
         if (!channel) return;
         let embed = new Discord.RichEmbed()
-        .setColor("#27AE60")
+        .setColor("#9A2EFE")
         .setThumbnail(memberavatar)
         .addField(':bust_in_silhouette: | name : ', `${member}`)
         .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
@@ -75,7 +75,7 @@ bot.on('guildMemberRemove', member => {
     let memberavatar = member.user.displayAvatarURL;
         if (!channel) return;
         let embed = new Discord.RichEmbed()
-        .setColor("#27AE60")
+        .setColor("#9A2EFE")
         .setThumbnail(memberavatar)
         .addField(':bust_in_silhouette: | Name:', `${member}`)
         .addField('Has Let the Server', 'Good Bye !')
