324346a1cff1029ea78f0892bf0f761534dcfd42
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 09:10:43 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
 let ping = Date.now() - message.createdTimestamp
 let embed = new Discord.RichEmbed()
-.setTitle(':dragon: Patrick Heard!')
+.setTitle('Your ping results:')
 .addField('Signal :signal_strength:',`${ping}ms`, true)
         .setColor("#9A2EFE")
 .setFooter('©Beta | By: Alfian Verter | 2018')
