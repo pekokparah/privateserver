5e178435fdb56694fd1e18eedc7b47810073a49a
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 13:30:32 +0700

    Update support.js

--- cmds/support.js
+++ cmds/support.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 
 
 exports.run = async (bot, message, args) => {
-message.channel.send('[tes](<https://discord.gg/YSpbDrp>)')
+message.channel.send('```click the link below to support our bot!```')
+message.channel.send('<https://discord.gg/YSpbDrp>')
 
 }
     
