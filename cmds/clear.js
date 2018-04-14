d38a695e1071e42299d693c2ec2b350803f180c3
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 09:08:54 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -15,8 +15,8 @@ exports.run = async (bot, message, args) => {
           return;
       }
     
-      if (insert > 400) {
-         message.channel.send("I just can delete 400 message")
+      if (insert > 100) {
+         message.channel.send("I just can delete 100 message")
       }
     
       const fetched = await message.channel.fetchMessages({limit: insert});
