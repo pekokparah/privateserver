671be7eb8dff90bd8a32e8f597ff20b06ba86c09
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 12:39:40 +0700

    Update announcement.js

--- cmds/announcement.js
+++ cmds/announcement.js
@@ -6,7 +6,7 @@ exports.run = async (bot, message, args) => {
 				
 		   const text = args.slice(1).join(" ");
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
-		   //const colour = args.slice(2).join("");
+		   const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor("0x" + color)
 		   .setTitle("Important Announcement:")
