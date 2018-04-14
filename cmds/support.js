cf530f5df136256a68358391f1b56a0a62641308
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 13:26:56 +0700

    Update support.js

--- cmds/support.js
+++ cmds/support.js
@@ -2,14 +2,7 @@ const Discord = require("discord.js");
 
 
 exports.run = async (bot, message, args) => {
-let embed = new Discord.RichEmbed()
-.setTitle('Support This Bot!')
-.setDescription('help and support this boot to continue to grow by way of vote !')
-.addField('Link','[Link Vote]<https://discordbots.org/bot/434304339268337665/vote>', true)
-.addField('Discord Official','<https://discord.gg/YSpbDrp>', true)
-        .setColor("#9A2EFE")
-.setFooter('©Beta | By: Alfian Verter | 2018')
-message.channel.send({embed})
+message.channel.send('<https://discord.gg/YSpbDrp>')
 
 }
     
