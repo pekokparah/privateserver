bad4ffa66e2124dec28e8c0ffb41f43b675129e9
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 07:47:35 +0700

    Update balance.js

--- cmds/balance.js
+++ cmds/balance.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
 
+exports.run = async (bot, message, args) => {
+
+          economy.fetchBalance(message.author.id + message.guild.id).then((i) => {
+            const embed = new Discord.RichEmbed()
+                .setDescription(`**${message.guild.name} Bank**`)
+                .setColor(0xD4AF37) 
+                .addField('Account Holder',message.author.username,true) 
+                .addField('Account Balance',i.money,true)
+
+
+            message.channel.send({embed})
+  
+}
+
+exports.help = {
+  name: "say"
+}
