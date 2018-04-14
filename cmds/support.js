2fee91236df45785120a1487f7ae350e87887588
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 13:17:14 +0700

    Create support.js

---
+++ cmds/support.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+
+exports.run = async (bot, message, args) => {
+let embed = new Discord.RichEmbed()
+.setTitle('Support This Bot!')
+.setDescription('help and support this boot to continue to grow by way of vote !')
+.addField('Link','[![Discord Bots](https://discordbots.org/api/widget/434304339268337665.svg)](https://discordbots.org/bot/434304339268337665)', true)
+        .setColor("#9A2EFE")
+.setFooter('©Beta | By: Alfian Verter | 2018')
+message.channel.send({embed})
+
+}
+    
+exports.help = {
+name: "invite"
+}
