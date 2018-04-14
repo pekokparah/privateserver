4d1493e7970db285cb56cc31a2da23545d431fb5
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 09:39:23 +0700

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -16,11 +16,9 @@ exports.run = async (bot, message, args) => {
     .addField("Tiime", message.createdAt)
     .addField("Reason", kReason);
 
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
+    
     message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
+    kUser.send(kickEmbed);
 }
 
 module.exports.help = {
