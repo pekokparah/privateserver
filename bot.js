d84d7da06adac350d1656026768ebdad667ea606
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 15:41:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const superagent = require("superagent");
 const weather = require("weather-js");
 const economy = require('discord-eco');
 const bot = new Discord.Client({disableEveryone: true});
+const moment = require("moment");
+const momentDurationFormat = require("moment-duration-format");
+
 
 
 var prefix = 'c.';
