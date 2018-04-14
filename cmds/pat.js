f93779c138c1148f489748737e77cac387fd95e8
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 13:00:50 +0700

    Update pat.js

--- cmds/pat.js
+++ cmds/pat.js
@@ -3,11 +3,11 @@ const superagent = require("superagent");
 
 exports.run = async (bot, message, args) => {
 let {body} = await superagent
-.get("https://nekos.life/api/pat")
+.get("nekos.life/api/pat")
 var catembed = new Discord.RichEmbed()
 .setTitle('Here is your random Pat')
         .setColor("#9A2EFE")
-.setImage(body.neko)
+.setImage(body.url)
 message.channel.send(catembed)
 }
 
