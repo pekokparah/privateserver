c33c3367a9044c9d73c17eaa815ac647764e910b
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 11:15:07 +0700

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -11,7 +11,7 @@ exports.run = async (bot, message, args) => {
     
   try{      
     let kickEmbed = new Discord.RichEmbed()
-    .setDescription("KICK !!")
+    .setDescription("BAN !!")
      .setColor("#9A2EFE")
     .addField("Kicked User", `${kUser}}`)
     .addField("Kicked By", `<@${message.author.id}>`)
