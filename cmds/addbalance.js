dcee22e21fa461d5bc38a61dc81072b0f8f58fbc
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 08:11:17 +0700

    Create addbalance.js

---
+++ cmds/addbalance.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const economy = require('discord-eco');
+
+exports.run = async (bot, message, args) => {
+
+        if (!args[0]) {
+            message.channel.send(`**You need to define an amount. Usage: ${prefix}addbalance <amount> <user>**`);
+            return;
+        }
+
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**The amount has to be a number. Usage: ${prefix}addbalance <amount> <user>**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+        }
+
+        // Check if they defined a user
+        let defineduser = '';
+        if (!args[1]) { // If they didn't define anyone, set it to their own.
+            defineduser = message.author.id;
+        } else { // Run this if they did define someone...
+            let firstMentioned = message.mentions.users.first();
+            defineduser = firstMentioned.id;
+        }
+
+        // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
+        economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
+            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
+        });
+
+    }
+
+exports.help = {
+  name: "say"
+}
