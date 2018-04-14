d7feca90509e1cadc014c96db26ebee76c4b3066
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-14 13:33:01 +0700

    Create votebot.js

---
+++ cmds/votebot.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+
+exports.run = async (bot, message, args) => {
+message.channel.send('```click the link below to vote our bot !```')
+message.channel.send('<https://discordbots.org/bot/434304339268337665/vote>')
+
+}
+    
+exports.help = {
+name: "support"
+}
