d467cc029dcecad847f4bc1c4948844f9d732bf8
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-15 22:19:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.on("ready", async () => {
     function randomStatus() {
         let status = [`on ${bot.guilds.size} servers.`, `with ${bot.users.size.toLocaleString()} users.`, 'mention @Cosmic', 'type )help']
           let rstatus = Math.floor(Math.random() * status.length);
-        bot.user.setActivity(status[rstatus], {type: 'STREAMING'});
+        bot.user.setActivity(status[rstatus], {type: 'WATCHING'});
     }; setInterval(randomStatus, 20000)
   });
   
